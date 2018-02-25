096be8578ea5fe179f35b3409de52fa532a29c4a
Jennifer Artschwager <Jennifer@artschwager.com>
2018-02-24 21:50:29 -0500

    JS added

--- Javascript/game.js
+++ Javascript/game.js
@@ -1,58 +1,86 @@
 
 
-//  a random number generates at the begining of the game.
-// each crystal represents a number that you can't see.  when you click the cyrstal it will add the worth amount to the total score.
-// click on the crystal to get the number that it's worth.
-//goal is to match the score to the random number.
-
-
-// when you win a different random number will appear.
-//the jems are different random numbers each time.
-// numbers random between 1-12 the guess number is random between 19-120 
-
-
-//add the points to the board
-
-
-//goal is to try to match the score to the random number.
-
-
-// mark wins
-
-//mark lossess
-
 var wins = 0;
 var losses = 0;
-var randomNumber = 0;
-var responseScore = 0;
-
-//place to store numbers
+var addingScore = 0;
 var randomCompNumbers = [];
+var goalNumber = 0;
 
-// functions to use the stored numbers
-function newNumbers() {
+
+function newGame() {
+    // set initial random number for the game
+    goalNumber= Math.floor(Math.random() * (50-25+1)+25);
+    console.log(goalNumber);
+
+    // calculating and creating the jem values
+    randomCompNumbers= [];
+    addingScore= 0;
+    //above puts the array back to empty before the new game.
+    //sets Jem values 0-3 randomCompNumbers[1] to call later
     for (i = 0; i < 4; i++) {
         var newRand= Math.floor(Math.random() * (12-1+1)+1);
         randomCompNumbers.push(newRand);
     }
     console.log(randomCompNumbers);
- 
+
+    $(".goalnumber").text(goalNumber);
+    $("#totalScore").text(addingScore);
+    $(".winsValue").text(wins);
+    $(".lossValue").text(losses);
 }
 
-
-newNumbers()
-
-// $("#wins")
- 
-// document.getElementById("losses").innerHTML = losses;
-
-function newRound() {
-    randomNumber = 0;
-    responseScore = 0;
+function addValue(jemID) {
+    addingScore += randomCompNumbers[jemID];
+    $("#totalScore").text(addingScore);
+    console.log(`addingScore: ${addingScore}, jemID: ${jemID}, jemValue: ${randomCompNumbers[jemID]}`);
+    // console.log(addingScore);
+    // console.log("jemID");
+    // console.log(jemID);
+    // console.log("jemValue");
+    // console.log(randomCompNumbers[jemID]);
+    if (addingScore === goalNumber) {
+        youWin();
+    } else {
+       if (addingScore > goalNumber) {
+            youLose();
+        }
+    }
 }
 
-if (randomNumber === responseScore) {
+function youWin() {
     wins += 1;
-    // document.getElementById("wins").innerHTML = wins;
-    newRound();
+    newGame();
+    // $(".winsValue").text(wins);
 }
+function youLose() {
+    losses += 1;
+    newGame();
+}
+
+    //do the on click for each jem
+    $(".blueJem").click(function() {
+        addValue(0);
+    });
+
+    $(".greenJem").click(function() {
+        addValue(1);
+    });
+    $(".orangeJem").click(function() {
+        addValue(2);
+    });
+    $(".purpleJem").click(function() {
+        addValue(3);
+    });
+
+
+
+
+// if (randomNumber === responseScore) {
+//     wins += 1;
+//     // document.getElementById("wins").innerHTML = wins;
+//     newRound();
+// }
+
+
+//this calls the new game.
+newGame()
